900b805d73be803f73f889fd86f27819ee9f38e5
yehudis raitman <yehudisr@gmail.com>
2021-01-17 21:54:08 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   count = 0 
-  while array.length < count do
+  while count <array.length do
   if array.include?(value_to_find) == true
     return array.index(value_to_find)
   end
